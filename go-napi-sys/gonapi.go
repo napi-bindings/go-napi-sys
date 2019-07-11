27f07ca537112aac42afc7007747ceafa332c154
NickNaso <nicoladelgobbo@gmail.com>
2019-07-11 16:40:18 +0200

    Added some explanations about error handling.

--- go-napi-sys/gonapi.go
+++ go-napi-sys/gonapi.go
@@ -105,6 +105,65 @@ type NapiThreadsafeFunctionCallJS C.napi_threadsafe_function_call_js
 // NapiNodeVersion represents ...
 type NapiNodeVersion C.napi_node_version
 
+// Error Handling
+// N-API uses both return values and JavaScript exceptions for error handling.
+// The following sections explain the approach for each case.
+// All of the N-API functions share the same error handling pattern. The return
+// type of all API functions is napi_status.
+// The return value will be napi_ok if the request was successful and no uncaught
+// JavaScript exception was thrown. If an error occurred AND an exception was
+// thrown, the napi_status value for the error will be returned. If an exception
+// was thrown, and no error occurred, napi_pending_exception will be returned.
+
+// In cases where a return value other than napi_ok or napi_pending_exception is
+// returned, napi_is_exception_pending must be called to check if an exception is
+// pending. See the section on exceptions for more details.
+
+// The napi_status return value provides a VM-independent representation of the
+// error which occurred. In some cases it is useful to be able to get more
+// detailed information, including a string representing the error as well as
+// VM (engine)-specific information.
+
+// Any N-API function call may result in a pending JavaScript exception. This is
+// obviously the case for any function that may cause the execution of
+// JavaScript, but N-API specifies that an exception may be pending on return
+// from any of the API functions. If the napi_status returned by a function is
+// napi_ok then no exception is pending and no additional action is required. If
+// the napi_status returned is anything other than napi_ok or
+// napi_pending_exception, in order to try to recover and continue instead of
+// simply returning immediately, napi_is_exception_pending must be called in
+// order to determine if an exception is pending or not. In many cases when an
+// N-API function is called and an exception is already pending, the function
+// will return immediately with a napi_status of napi_pending_exception.
+// However, this is not the case for all functions. N-API allows a subset of the
+// functions to be called to allow for some minimal cleanup before returning to
+// JavaScript. In that case, napi_status will reflect the status for the
+// function. It will not reflect previous pending exceptions. To avoid confusion,
+// check the error status after every function call.
+
+// When an exception is pending one of two approaches can be employed.:
+// The first approach is to do any appropriate cleanup and then return so that
+// execution will return to JavaScript. As part of the transition back to
+// JavaScript the exception will be thrown at the point in the JavaScript code
+// where the native method was invoked. The behavior of most N-API calls is
+// unspecified while an exception is pending, and many will simply return
+// napi_pending_exception, so it is important to do as little as possible and
+// then return to JavaScript where the exception can be handled.
+// The second approach is to try to handle the exception. There will be cases
+// where the native code can catch the exception, take the appropriate action,
+// and then continue. This is only recommended in specific cases where it is
+// known that the exception can be safely handled.
+
+// The Node.js project is adding error codes to all of the errors generated
+// internally. The goal is for applications to use these error codes for all
+// error checking. The associated error messages will remain, but will only be
+// meant to be used for logging and display with the expectation that the message
+// can change without SemVer applying. In order to support this model with N-API,
+// both in internal functionality and for module specific functionality
+// (as its good practice), the throw_ and create_ functions take an optional code
+// parameter which is the string for the code to be added to the error object. If
+// the optional parameter is NULL then no code will be associated with the error.
+
 // NapiGetLastErrorInfo function ...
 // [in] env: The environment that the API is invoked under.
 // This API retrieves a napi_extended_error_info structure with information about
