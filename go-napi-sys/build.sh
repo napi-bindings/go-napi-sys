ed2eab6358dcbe1884b89f6363b21e33f4233888
NickNaso <nicoladelgobbo@gmail.com>
2019-08-19 23:58:57 +0200

    Set GOPATH

--- go-napi-sys/build.sh
+++ go-napi-sys/build.sh
@@ -19,6 +19,8 @@ cp ../napi-stub/node_api.h include/node_api.h && \
 echo Start building ... && \
 # Remember for Node.js version less than 12 the MACOSX_DEPLOYMENT_TARGET need to 
 # be set to 10.7
-export MACOSX_DEPLOYMENT_TARGET=10.10 && go build -a -x -o libgoaddon.a -buildmode=c-archive . && \
+export MACOSX_DEPLOYMENT_TARGET=10.10 && \
+export GOPATH=$(pwd) && \
+go build -a -x -o libgoaddon.a -buildmode=c-archive . && \
 echo Build finished.
 
